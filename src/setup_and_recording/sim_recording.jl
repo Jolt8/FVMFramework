cec946313d28aaa8709ad31e0e1c6a9102074731
Jolt8 <willemartin@icloud.com>
2026-02-25 11:39:50 -0600

    This has been updated to actually drill into the fields present with sol.u. This will have to be changed later when SubArrays from converting ComponentVectors to NamedTuples is no longer and issue.

--- src/setup_and_recording/sim_recording.jl
+++ src/setup_and_recording/sim_recording.jl
@@ -1,3 +1,23 @@
+function write_to_vtk_helper!(vtk, u_named_step, n_cells)
+    for field in keys(u_named_step)
+        data = u_named_step[field]
+
+        if data isa ComponentVector || data isa NamedTuple
+            write_to_vtk_helper!(vtk, data, n_cells)
+        elseif data isa Vector && length(data) == n_cells 
+            #the length check is to handle cases where a field is not present on all cells, which write_to_vtk wouldn't accept
+            #this is tricky because we can't exactly write stuff to cells that they're not on
+            #one way to handle this is to create an array of length n_cells and then fill it with zeros and then write to the respective cell_ids of the field
+            #this doens't work for things that are indexed by controller_id for example, which is what the current logic is handling
+            write_cell_data(vtk, data, String(field))
+        elseif data isa SubArray
+            write_cell_data(vtk, data[1], String(field))
+        else
+            println("not processed: ", field)
+        end
+    end
+end
+
 function sol_to_vtk(sol, u_named, grid, sim_file)
     date_and_time = Dates.format(now(), "I.MM.SS p yyyy-mm-dd")
     #date_and_time = Dates.format(now(), "I.MM.SS p")
@@ -22,27 +42,11 @@ function sol_to_vtk(sol, u_named, grid, sim_file)
     #update: This is a very big bottleneck
     #we get a component array of the simulation's state at this step
 
+    n_cells = length(grid.cells)
+
     for (step, t) in enumerate(sol.t)
         VTKGridFile(step_filename * " $step" * " at $t.vtu", grid) do vtk
-            for field in propertynames(u_named[step])
-                data = u_named[step][field]
-                if field != :mass_fractions
-                    write_cell_data(vtk, data, String(field))
-                else
-                    if data isa ComponentVector
-                        for species in propertynames(data)
-                            write_cell_data(vtk, data[species], String(species))
-                        end
-                    elseif ndims(data) == 2 # Matrix of (species, cells) 
-                        for i in eachindex(data[:, 1])
-                            write_cell_data(vtk, data[i, :], "mass_fraction_$i")
-                        end
-                    else
-                        # Fallback for other structures
-                        write_cell_data(vtk, data, String(field))
-                    end
-                end
-            end
+            write_to_vtk_helper!(vtk, u_named[step], n_cells)
             pvd[t] = vtk
         end
     end
