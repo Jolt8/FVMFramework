82f20c8d943db747aade315e579af49fabeeb0ca
Jolt8 <willemartin@icloud.com>
2026-03-06 17:07:12 -0600

    this still needs some work

--- examples/Methanol_Reformer/non-FVM related work/1D physics/empirical_hx_correlations.jl
+++ examples/Methanol_Reformer/non-FVM related work/1D physics/empirical_hx_correlations.jl
@@ -14,3 +14,58 @@ function Nu_packed_bed_Gnielinski!(du, u, cell_id, vol)
     
     u.Nu = Nu_sphere * fa
 end
+
+function calculate_UA_prime_system(du, u, cell_id, vol)
+    Re_1 = (u.rho[cell_id] * u.velocity[cell_id] * u.channel_hydraulic_diameter[cell_id]) / (u.viscosity[cell_id])
+    Pr_1 = (u.viscosity[cell_id] * u.cp[cell_id]) / (u.pipe_k[cell_id])
+
+    Re2f_1 = u.Re2f_interpolation
+    if Re_1 < 2200
+        fd_1 = 64/Re_1 # Laminar approximation
+    else
+        fd_1 = Re2f_1(Re_1)
+    end
+
+    # -- Pipe 2 -- 
+    fluid_2_thermal_conductivity = 0.034u"W/(m*K)"
+
+    fluid_2_molar_density = GasProperties.get_mixture_molar_density(model_2_T, model_2_P)
+    fluid_2_density = GasProperties.get_mixture_density(model_2.coolprop_components, model_2_common.z_vec, model_2_T, model_2_P)
+
+    #println("model_1_T, ", model_1_T)
+    println("model_2_T, ", model_2_T)
+    #println("model_2_common.z_vec, ", model_2_common.z_vec)
+    fluid_2_specific_heat = GasProperties.get_mixture_cp_mass(model_2.coolprop_components, model_2_common.z_vec, model_2_T)
+
+    fluid_2_total_molar_flow = sum(model_2_molar_flows)
+
+    fluid_2_total_volumetric_flow = fluid_2_total_molar_flow / fluid_2_molar_density
+
+    fluid_2_velocity = fluid_2_total_volumetric_flow / model_2.pressure_model.channel_cross_sectional_area
+
+    fluid_2_dynamic_viscosity = 3.5e-5u"Pa*s" #just using something close
+
+    Re_2 = (fluid_2_density * fluid_2_velocity * model_2.pressure_model.channel_hydraulic_diameter) / (fluid_2_dynamic_viscosity)
+    #println("fluid_2_dynamic_viscosity ", fluid_2_dynamic_viscosity)
+    #println("fluid_2_specific_heat ", fluid_2_specific_heat)
+    #println("fluid_2_thermal_conductivity ", fluid_2_thermal_conductivity)
+    Pr_2 = (fluid_2_dynamic_viscosity * fluid_2_specific_heat) / (fluid_2_thermal_conductivity)
+    #println("Pr_2 ", Pr_2)
+
+    Re2f_2 = model_2.pressure_model.Re2f_interpolation
+    if Re_2 < 2200
+        fd_2 = 64/Re_2 # Laminar approximation
+    else
+        fd_2 = Re2f_2(Re_2)
+    end
+
+    phys_diameter = model_1.pressure_model.channel_hydraulic_diameter * (pi + 2) / pi
+
+    return get_linear_heat_transfer_coefficient(
+        z_position, 
+        model_1.pressure_model.channel_hydraulic_diameter, fluid_1_thermal_conductivity, Re_1, Pr_1, fd_1, 
+        model_2.pressure_model.channel_hydraulic_diameter, fluid_2_thermal_conductivity, Re_2, Pr_2, fd_2,
+        phys_diameter,
+        layers
+    )
+end
