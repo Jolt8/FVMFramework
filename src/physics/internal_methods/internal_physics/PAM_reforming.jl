74b0937cddc13d71535babbdfadae8b99d7a0570
Jolt8 <willemartin@icloud.com>
2026-03-08 22:49:56 -0600

    Added a condition to make sure that PAM_reforming doesn't activate when only the initial inert gas is present. Hopefully the if statement isn't that bad for performance.

--- src/physics/internal_methods/internal_physics/PAM_reforming.jl
+++ src/physics/internal_methods/internal_physics/PAM_reforming.jl
@@ -3,130 +3,144 @@ function PAM_reforming_react_cell!(du, u, cell_id, vol)
     rho_ideal!(u, cell_id)
     molar_concentrations!(u, cell_id)
 
+    
     #we divide by 1e-5 because PAM parameters are in bar
+    #we add a tiny epsilon (1e-12) to prevent division by zero in the denominators/roots
+    #during the start-up phase when concentrations are close to 0.0
+
     conversion_factor = ((R_gas * u.temp[cell_id]) * 1e-5)
-    P_CH3OH = u.molar_concentrations.methanol[cell_id] * conversion_factor
-    P_H2O = u.molar_concentrations.water[cell_id] * conversion_factor
-    P_CO = u.molar_concentrations.carbon_monoxide[cell_id] * conversion_factor
-    P_H2 = u.molar_concentrations.hydrogen[cell_id] * conversion_factor
-    P_CO2 = u.molar_concentrations.carbon_dioxide[cell_id] * conversion_factor
+    eps = 1e-12
+    P_CH3OH = max(u.molar_concentrations.methanol[cell_id] * conversion_factor, eps)
+    P_H2O = max(u.molar_concentrations.water[cell_id] * conversion_factor, eps)
+    P_CO = max(u.molar_concentrations.carbon_monoxide[cell_id] * conversion_factor, eps)
+    P_H2 = max(u.molar_concentrations.hydrogen[cell_id] * conversion_factor, eps)
+    P_CO2 = max(u.molar_concentrations.carbon_dioxide[cell_id] * conversion_factor, eps)
 
-    #=
-    println("P_CH3OH: $(P_CH3OH)")
-    println("P_H2O: $(P_H2O)")
-    println("P_CO: $(P_CO)")
-    println("P_H2: $(P_H2)")
-    println("P_CO2: $(P_CO2)")
-    println("")
-    =#
+    if P_H2 > 1e-8 && P_CH3OH > 1e-8
 
-    K_CH3O = van_t_hoff(
-        u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_A.CH3O[cell_id], 
-        u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_dH.CH3O[cell_id], 
-        u.temp[cell_id]
-    )
-    K_HCOO = van_t_hoff(
-        u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_A.HCOO[cell_id], 
-        u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_dH.HCOO[cell_id], 
-        u.temp[cell_id]
-    )
-    K_OH = van_t_hoff(
-        u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_A.OH[cell_id], 
-        u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_dH.OH[cell_id], 
-        u.temp[cell_id]
-    )
+        #=
+        println("P_CH3OH: $(P_CH3OH)")
+        println("P_H2O: $(P_H2O)")
+        println("P_CO: $(P_CO)")
+        println("P_H2: $(P_H2)")
+        println("P_CO2: $(P_CO2)")
+        println("")
+        =#
 
-    #=
-    println("K_CH3O: $(K_CH3O)")
-    println("K_HCOO: $(K_HCOO)")
-    println("K_OH: $(K_OH)")
-    println("")
-    =#
+        K_CH3O = van_t_hoff(
+            u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_A.CH3O[cell_id], 
+            u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_dH.CH3O[cell_id], 
+            u.temp[cell_id]
+        )
+        K_HCOO = van_t_hoff(
+            u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_A.HCOO[cell_id], 
+            u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_dH.HCOO[cell_id], 
+            u.temp[cell_id]
+        )
+        K_OH = van_t_hoff(
+            u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_A.OH[cell_id], 
+            u.reactions.reforming_reactions.MSR_rxn.van_t_hoff_dH.OH[cell_id], 
+            u.temp[cell_id]
+        )
 
-    term_CH3O = K_CH3O * (P_CH3OH / sqrt(P_H2))
-    term_HCOO = K_HCOO * (P_CO2 * sqrt(P_H2))
-    term_OH = K_OH * (P_H2O / sqrt(P_H2))
+        #=
+        println("K_CH3O: $(K_CH3O)")
+        println("K_HCOO: $(K_HCOO)")
+        println("K_OH: $(K_OH)")
+        println("")
+        =#
 
-    #=
-    println("term_CH3O: $(term_CH3O)")
-    println("term_HCOO: $(term_HCOO)")
-    println("term_OH: $(term_OH)")
-    println("")
-    =#
+        term_CH3O = K_CH3O * (P_CH3OH / sqrt(P_H2))
+        term_HCOO = K_HCOO * (P_CO2 * sqrt(P_H2))
+        term_OH = K_OH * (P_H2O / sqrt(P_H2))
 
-    DEN = 1.0 + term_CH3O + term_HCOO + term_OH
+        #=
+        println("term_CH3O: $(term_CH3O)")
+        println("term_HCOO: $(term_HCOO)")
+        println("term_OH: $(term_OH)")
+        println("")
+        =#
 
-    #println("DEN: $(DEN)")
-    #println("")
+        DEN = 1.0 + term_CH3O + term_HCOO + term_OH
 
-    map(keys(u.reactions.reforming_reactions)) do reforming_reaction
-        u.net_rates.reforming_reactions[reforming_reaction][1] = 0.0
-    end
+        #println("DEN: $(DEN)")
+        #println("")
 
-    #MSR
-    k_MSR = u.reactions.reforming_reactions.MSR_rxn.kf_A[cell_id] * exp(-u.reactions.reforming_reactions.MSR_rxn.kf_Ea[cell_id] / (R_gas * u.temp[cell_id]))
-    K_eq_MSR = K_gibbs_free(u, cell_id, u.reactions.reforming_reactions.MSR_rxn)
-
-    driving_force = 1.0 - ((P_CO2 * P_H2^3) / (K_eq_MSR * P_CH3OH * P_H2O))
-
-    u.net_rates.reforming_reactions.MSR_rxn[1] = (k_MSR * K_CH3O * (P_CH3OH / sqrt(P_H2)) * driving_force) / (DEN^2)
-    
-    #=
-    println("R_gas: $(R_gas)")
-    println("temp: $(u.temp[cell_id])")
-    println("R_gas * temp: $(R_gas * u.temp[cell_id])")
-    println("kf_Ea: $(u.reactions.reforming_reactions.MSR_rxn.kf_Ea[cell_id])")
-    println("exp(-kf_Ea / (R_gas * temp)): $(exp(-u.reactions.reforming_reactions.MSR_rxn.kf_Ea[cell_id] / (R_gas * u.temp[cell_id])))")
-    println("kf_A: $(u.reactions.reforming_reactions.MSR_rxn.kf_A[cell_id])")
-    println("k_MSR: $(k_MSR)")    
-    
-    println("K_eq_MSR: $(K_eq_MSR)")
-    println("driving_force: $(driving_force)")
-    println("u.net_rates.reforming_reactions.MSR_rxn[1]: $(u.net_rates.reforming_reactions.MSR_rxn[1])")
-    println("")
-    =#
-
-    #MD
-    k_MD = u.reactions.reforming_reactions.MD_rxn.kf_A[cell_id] * exp(-u.reactions.reforming_reactions.MD_rxn.kf_Ea[cell_id] / (R_gas * u.temp[cell_id]))
-    K_eq_MD = K_gibbs_free(u, cell_id, u.reactions.reforming_reactions.MD_rxn)
-    driving_force = 1.0 - ((P_CO * P_H2^2) / (K_eq_MD * P_CH3OH))
-
-    u.net_rates.reforming_reactions.MD_rxn[1] = (k_MD * K_CH3O * (P_CH3OH / sqrt(P_H2)) * driving_force) / (DEN^2)
-
-    #=
-    println("k_MD: $(k_MD)")
-    println("K_eq_MD: $(K_eq_MD)")
-    println("driving_force: $(driving_force)")
-    println("u.net_rates.reforming_reactions.MD_rxn[1]: $(u.net_rates.reforming_reactions.MD_rxn[1])")
-    println("")
-    =#
-
-    #WGS
-    k_WGS = u.reactions.reforming_reactions.WGS_rxn.kf_A[cell_id] * exp(-u.reactions.reforming_reactions.WGS_rxn.kf_Ea[cell_id] / (R_gas * u.temp[cell_id]))
-    K_eq_WGS = K_gibbs_free(u, cell_id, u.reactions.reforming_reactions.WGS_rxn)
-    driving_force = 1.0 - ((P_CO2 * P_H2) / (K_eq_WGS * P_CO * P_H2O))
-
-    u.net_rates.reforming_reactions.WGS_rxn[1] = (k_WGS * K_OH * ((P_CO * P_H2O) / sqrt(P_H2)) * driving_force) / (DEN^2)
-
-    #=
-    println("k_WGS: $(k_WGS)")
-    println("K_eq_WGS: $(K_eq_WGS)")
-    println("driving_force: $(driving_force)")
-    println("u.net_rates.reforming_reactions.WGS_rxn[1]: $(u.net_rates.reforming_reactions.WGS_rxn[1])")
-    println("")
-    =#
-
-    map(keys(u.species_ids)) do species_name
-        du.molar_concentrations[species_name][cell_id] = 0.0
         map(keys(u.reactions.reforming_reactions)) do reforming_reaction
-            du.molar_concentrations[species_name][cell_id] += u.net_rates.reforming_reactions[reforming_reaction][1] * u.reactions.reforming_reactions[reforming_reaction].stoich_coeffs[species_name][cell_id] * u.reforming_reactions[reforming_reaction].reactions_kg_cat
+            u.net_rates.reforming_reactions[reforming_reaction][1] = 0.0
         end
-        du.mass_fractions[species_name][cell_id] += (du.molar_concentrations[species_name][cell_id] * u.molecular_weights[species_name][cell_id] / u.rho[cell_id])
-        # rate (mol/(m3*s)) * MW (g/mol) / rho (g/m3) = unitless/s
-    end
 
-    map(keys(u.reactions.reforming_reactions)) do reforming_reaction
-        du.heat[cell_id] += u.net_rates.reforming_reactions[reforming_reaction][1] * (-u.reactions.reforming_reactions[reforming_reaction].heat_of_reaction[cell_id]) * vol 
-        # rate (mol/(m3*s)) * H (J/mol) * vol (m3) = J/s = Watts
+        #MSR
+        k_MSR = u.reactions.reforming_reactions.MSR_rxn.kf_A[cell_id] * exp(-u.reactions.reforming_reactions.MSR_rxn.kf_Ea[cell_id] / (R_gas * u.temp[cell_id]))
+        K_eq_MSR = K_gibbs_free(u, cell_id, u.reactions.reforming_reactions.MSR_rxn)
+
+        driving_force = 1.0 - ((P_CO2 * P_H2^3) / (K_eq_MSR * P_CH3OH * P_H2O))
+
+        u.net_rates.reforming_reactions.MSR_rxn[1] = (k_MSR * K_CH3O * (P_CH3OH / sqrt(P_H2)) * driving_force) / (DEN^2)
+        
+        #=
+        println("R_gas: $(R_gas)")
+        println("temp: $(u.temp[cell_id])")
+        println("R_gas * temp: $(R_gas * u.temp[cell_id])")
+        println("kf_Ea: $(u.reactions.reforming_reactions.MSR_rxn.kf_Ea[cell_id])")
+        println("exp(-kf_Ea / (R_gas * temp)): $(exp(-u.reactions.reforming_reactions.MSR_rxn.kf_Ea[cell_id] / (R_gas * u.temp[cell_id])))")
+        println("kf_A: $(u.reactions.reforming_reactions.MSR_rxn.kf_A[cell_id])")
+        println("k_MSR: $(k_MSR)")    
+        
+        println("K_eq_MSR: $(K_eq_MSR)")
+        println("driving_force: $(driving_force)")
+        println("u.net_rates.reforming_reactions.MSR_rxn[1]: $(u.net_rates.reforming_reactions.MSR_rxn[1])")
+        println("")
+        =#
+
+        #MD
+        k_MD = u.reactions.reforming_reactions.MD_rxn.kf_A[cell_id] * exp(-u.reactions.reforming_reactions.MD_rxn.kf_Ea[cell_id] / (R_gas * u.temp[cell_id]))
+        K_eq_MD = K_gibbs_free(u, cell_id, u.reactions.reforming_reactions.MD_rxn)
+        driving_force = 1.0 - ((P_CO * P_H2^2) / (K_eq_MD * P_CH3OH))
+
+        u.net_rates.reforming_reactions.MD_rxn[1] = (k_MD * K_CH3O * (P_CH3OH / sqrt(P_H2)) * driving_force) / (DEN^2)
+
+        #=
+        println("k_MD: $(k_MD)")
+        println("K_eq_MD: $(K_eq_MD)")
+        println("driving_force: $(driving_force)")
+        println("u.net_rates.reforming_reactions.MD_rxn[1]: $(u.net_rates.reforming_reactions.MD_rxn[1])")
+        println("")
+        =#
+
+        #WGS
+        k_WGS = u.reactions.reforming_reactions.WGS_rxn.kf_A[cell_id] * exp(-u.reactions.reforming_reactions.WGS_rxn.kf_Ea[cell_id] / (R_gas * u.temp[cell_id]))
+        K_eq_WGS = K_gibbs_free(u, cell_id, u.reactions.reforming_reactions.WGS_rxn)
+        driving_force = 1.0 - ((P_CO2 * P_H2) / (K_eq_WGS * P_CO * P_H2O))
+
+        u.net_rates.reforming_reactions.WGS_rxn[1] = (k_WGS * K_OH * ((P_CO * P_H2O) / sqrt(P_H2)) * driving_force) / (DEN^2)
+
+        #=
+        println("k_WGS: $(k_WGS)")
+        println("K_eq_WGS: $(K_eq_WGS)")
+        println("driving_force: $(driving_force)")
+        println("u.net_rates.reforming_reactions.WGS_rxn[1]: $(u.net_rates.reforming_reactions.WGS_rxn[1])")
+        println("")
+        =#
+
+        map(keys(u.molar_concentrations)) do species_name
+            du.molar_concentrations[species_name][cell_id] = 0.0
+        end
+
+        map(keys(u.reactions.reforming_reactions)) do reforming_reaction
+            map(keys(u.reactions.reforming_reactions[reforming_reaction].stoich_coeffs)) do species_name
+                du.molar_concentrations[species_name][cell_id] += u.net_rates.reforming_reactions[reforming_reaction][1] * u.reactions.reforming_reactions[reforming_reaction].stoich_coeffs[species_name][cell_id] * u.reactions_kg_cat.reforming_reactions[reforming_reaction][cell_id]
+            end
+        end
+
+        map(keys(u.mass_fractions)) do species_name
+            du.mass_fractions[species_name][cell_id] += (du.molar_concentrations[species_name][cell_id] * u.molecular_weights[species_name][cell_id] / u.rho[cell_id])
+            # rate (mol/(m3*s)) * MW (g/mol) / rho (g/m3) = unitless/s
+        end
+
+        map(keys(u.reactions.reforming_reactions)) do reforming_reaction
+            du.heat[cell_id] += u.net_rates.reforming_reactions[reforming_reaction][1] * (-u.reactions.reforming_reactions[reforming_reaction].heat_of_reaction[cell_id]) * vol 
+            # rate (mol/(m3*s)) * H (J/mol) * vol (m3) = J/s = Watts
+        end
     end
 end
