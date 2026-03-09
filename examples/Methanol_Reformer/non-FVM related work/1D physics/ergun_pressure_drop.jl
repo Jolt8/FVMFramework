58caaf58284ad858f6e00611aba039954c8e5734
Jolt8 <willemartin@icloud.com>
2026-03-08 22:31:16 -0600

    This still needs some work. I think the best option would be to pre-compute the pressure gradient and then apply that to each cell based on an interpolation.

--- examples/Methanol_Reformer/non-FVM related work/1D physics/ergun_pressure_drop.jl
+++ examples/Methanol_Reformer/non-FVM related work/1D physics/ergun_pressure_drop.jl
@@ -1,9 +1,8 @@
 function ergun_pressure_drop!(du, u, cell_id, vol)
-    #superficial_mass_velocity = du.mass_face[cell_id][3] / u.pipe_area[cell_id]
-
     term1 = 150 * (1 - u.bed_void_fraction[cell_id])^2 / u.bed_void_fraction[cell_id]^3 * u.viscosity[cell_id] * u.superficial_mass_velocity[cell_id] / (u.rho[cell_id] * u.catalyst_particle_diameter[cell_id]^2)
     term2 = 1.75 * (1 - u.bed_void_fraction[cell_id]) / u.bed_void_fraction[cell_id]^3 * u.superficial_mass_velocity[cell_id]^2 / (u.catalyst_particle_diameter[cell_id] * u.rho[cell_id])
     pressure_drop = (term1 + term2) * u.pipe_length[cell_id]
     
-    du.pressure[cell_id] -= pressure_drop
+    du.pressure[cell_id] -= pressure_drop 
+    #this is incorrect, ergun is spatial while this is temporal, getting it to be spatial is going to be quite trickly
 end
