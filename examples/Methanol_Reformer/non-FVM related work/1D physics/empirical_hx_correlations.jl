ba690ae4a09f31bf632c47d7ca982066f5d9a698
Jolt8 <willemartin@icloud.com>
2026-03-08 22:30:36 -0600

    This now works properly!

--- examples/Methanol_Reformer/non-FVM related work/1D physics/empirical_hx_correlations.jl
+++ examples/Methanol_Reformer/non-FVM related work/1D physics/empirical_hx_correlations.jl
@@ -1,71 +1,41 @@
-function Nu_packed_bed_Gnielinski!(du, u, cell_id, vol)
-    #borrowed from ht.py
+function Nu_packed_bed_Gnielinski!(du, u, cell_id, Re, Pr)
+    #borrowed from ht.py (love that package)
     #Calculates Nusselt number of a fluid passing over a bed of particles
-
-    Re = u.rho * u.viscosity * u.catalyst_particle_diameter / u.mu / u.bed_void_fraction
     
-    Nu_lam = 0.664 * Re^0.5 * u.Pr^(1/3.)
+    Nu_lam = 0.664 * Re^0.5 * Pr^(1/3.)
     
-    Nu_turb = 0.037 * Re^0.8 * u.Pr/(1 + 2.443 * Re^-0.1 * (u.Pr^(2/3.) - 1))
+    Nu_turb = 0.037 * Re^0.8 * Pr/(1 + 2.443 * Re^-0.1 * (Pr^(2/3.) - 1))
     
     Nu_sphere = 2 + (Nu_lam^2 + Nu_turb^2)^0.5
     
-    fa = 1.0 + 1.5 * (1.0 - u.bed_void_fraction)
+    fa = 1.0 + 1.5 * (1.0 - u.bed_void_fraction[cell_id])
+
+    Nu = Nu_sphere * fa
     
-    u.Nu = Nu_sphere * fa
+    return Nu
 end
 
-function calculate_UA_prime_system(du, u, cell_id, vol)
-    Re_1 = (u.rho[cell_id] * u.velocity[cell_id] * u.channel_hydraulic_diameter[cell_id]) / (u.viscosity[cell_id])
-    Pr_1 = (u.viscosity[cell_id] * u.cp[cell_id]) / (u.pipe_k[cell_id])
+function UA_prime(du, u, cell_id, vol)
+    #println("u.rho[cell_id] = $(u.rho[cell_id])")
+    #println("u.velocity[cell_id] = $(u.velocity[cell_id])")
+    #println("u.pipe_hydraulic_diameter[cell_id] = $(u.pipe_hydraulic_diameter[cell_id])")
+    #println("u.viscosity[cell_id] = $(u.viscosity[cell_id])")
+    #println("u.cp[cell_id] = $(u.cp[cell_id])")
+    #println("u.k[cell_id] = $(u.k[cell_id])")
+    Re = (u.rho[cell_id] * u.velocity[cell_id] * u.pipe_hydraulic_diameter[cell_id]) / (u.viscosity[cell_id])
+    #println("Re = $(Re)")
+    Pr = (u.viscosity[cell_id] * u.cp[cell_id]) / (u.k[cell_id])
+    #println("Pr = $(Pr)")
 
-    Re2f_1 = u.Re2f_interpolation
-    if Re_1 < 2200
-        fd_1 = 64/Re_1 # Laminar approximation
-    else
-        fd_1 = Re2f_1(Re_1)
-    end
+    pipe_nusselt_number = Nu_packed_bed_Gnielinski!(du, u, cell_id, Re, Pr)
 
-    # -- Pipe 2 -- 
-    fluid_2_thermal_conductivity = 0.034u"W/(m*K)"
+    pipe_heat_transfer_coeff = (pipe_nusselt_number * u.cp[cell_id]) / u.pipe_hydraulic_diameter[cell_id]
 
-    fluid_2_molar_density = GasProperties.get_mixture_molar_density(model_2_T, model_2_P)
-    fluid_2_density = GasProperties.get_mixture_density(model_2.coolprop_components, model_2_common.z_vec, model_2_T, model_2_P)
+    conv_resistance = 1 / (pipe_heat_transfer_coeff * u.pipe_hydraulic_diameter[cell_id])
 
-    #println("model_1_T, ", model_1_T)
-    println("model_2_T, ", model_2_T)
-    #println("model_2_common.z_vec, ", model_2_common.z_vec)
-    fluid_2_specific_heat = GasProperties.get_mixture_cp_mass(model_2.coolprop_components, model_2_common.z_vec, model_2_T)
+    total_cond_resistance = u.pipe_thickness[cell_id] / (u.pipe_k[cell_id] * u.pipe_hydraulic_diameter[cell_id])
 
-    fluid_2_total_molar_flow = sum(model_2_molar_flows)
+    UA_prime = 1 / (conv_resistance + total_cond_resistance)
 
-    fluid_2_total_volumetric_flow = fluid_2_total_molar_flow / fluid_2_molar_density
-
-    fluid_2_velocity = fluid_2_total_volumetric_flow / model_2.pressure_model.channel_cross_sectional_area
-
-    fluid_2_dynamic_viscosity = 3.5e-5u"Pa*s" #just using something close
-
-    Re_2 = (fluid_2_density * fluid_2_velocity * model_2.pressure_model.channel_hydraulic_diameter) / (fluid_2_dynamic_viscosity)
-    #println("fluid_2_dynamic_viscosity ", fluid_2_dynamic_viscosity)
-    #println("fluid_2_specific_heat ", fluid_2_specific_heat)
-    #println("fluid_2_thermal_conductivity ", fluid_2_thermal_conductivity)
-    Pr_2 = (fluid_2_dynamic_viscosity * fluid_2_specific_heat) / (fluid_2_thermal_conductivity)
-    #println("Pr_2 ", Pr_2)
-
-    Re2f_2 = model_2.pressure_model.Re2f_interpolation
-    if Re_2 < 2200
-        fd_2 = 64/Re_2 # Laminar approximation
-    else
-        fd_2 = Re2f_2(Re_2)
-    end
-
-    phys_diameter = model_1.pressure_model.channel_hydraulic_diameter * (pi + 2) / pi
-
-    return get_linear_heat_transfer_coefficient(
-        z_position, 
-        model_1.pressure_model.channel_hydraulic_diameter, fluid_1_thermal_conductivity, Re_1, Pr_1, fd_1, 
-        model_2.pressure_model.channel_hydraulic_diameter, fluid_2_thermal_conductivity, Re_2, Pr_2, fd_2,
-        phys_diameter,
-        layers
-    )
+    return UA_prime
 end
