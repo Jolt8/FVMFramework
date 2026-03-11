daf086a31528f119b59aa2f337d308e8a2208a1c
Jolt8 <willemartin@icloud.com>
2026-03-11 08:11:38 -0600

    arrenhius_mass_fraction_diffusion now uses the new species_mass_flows which is then used to find the change in mass fractions.

--- examples/Methylene Blue Diffusion Parameter Fitting/drug-eluting hydrogel implants.jl/arrenhius_mass_fraction_diffusion_meth_blue_and_water.jl
+++ examples/Methylene Blue Diffusion Parameter Fitting/drug-eluting hydrogel implants.jl/arrenhius_mass_fraction_diffusion_meth_blue_and_water.jl
@@ -23,29 +23,6 @@ function arrenhius_mass_fraction_diffusion_meth_blue_and_water!(
     meth_blue_concentration_gradient = (u.mass_fractions.methylene_blue[idx_b] - u.mass_fractions.methylene_blue[idx_a]) / dist
     meth_blue_diffusion = -rho_avg * meth_blue_species_diffusion_coefficient * meth_blue_concentration_gradient * area
 
-    #I should probably check the units here
-
-    #we should probably divide by rho * vol in a capacity function instead later
-    #if du.mass_fractions.methylene_blue[idx_a] > 1.0
-        #println(du.mass_fractions.methylene_blue[idx_a])
-    #end
-    #if du.mass_fractions.wateridx_a] > 1.0
-        #println(du.mass_fractions.water[idx_a])
-    #end
-    du.mass_fractions.methylene_blue[idx_a] -= meth_blue_diffusion / (u.rho[idx_a] * vol_a)
-    du.mass_fractions.water[idx_a] += meth_blue_diffusion / (u.rho[idx_a] * vol_a)
-    
-    #=
-    if idx_a == 64 
-        println("64 as idx_a was applied to")
-    elseif idx_a == 1008
-        println("1008 as idx_a was applied to")
-    end
-
-    if idx_b == 64 
-        println("64 as idx_b was applied to")
-    elseif idx_b == 1008
-        println("1008 as idx_b was applied to")
-    end
-    =#
+    du.species_mass_flows.methylene_blue[idx_a] -= meth_blue_diffusion
+    du.species_mass_flows.water[idx_a] += meth_blue_diffusion
 end
