2c7e28943a444390781dce4329175bab9d955785
Jolt8 <willemartin@icloud.com>
2026-03-17 21:19:08 -0600

    created a custom method for looping over nested fields in ComponentArrays
    best_componentarrays_looping_style is my favourite out of the three examples. I'll probably use this in the actual solver.

--- small_tests/in_house_temporary/best_componentarrays_looping_style.jl
+++ small_tests/in_house_temporary/best_componentarrays_looping_style.jl
@@ -3,13 +3,31 @@ using ComponentArrays
 @generated function foreach_field_at!(f, cell_id::Int, groups::Vararg{ComponentVector, N}) where {N}
     first_ax = groups[1].parameters[4].parameters[1]
     
-    #the first parameter passed in is assumed to be the NamedTuple containing the layout
+    #the first parameter passed in is assumed to contain the same axes of the other groups passed in
     properties = keys(first_ax.parameters[1]) 
     
     exprs = []
     for n in properties
         view_exprs = [:(getproperty(groups[$i], $(QuoteNode(n)))) for i in 1:N]
         push!(exprs, :(f(cell_id, $(view_exprs...))))
+    end
+    
+    return quote
+        Base.@_inline_meta
+        $(exprs...)
+        nothing
+    end
+end
+
+@generated function foreach_field_at!(f, groups::Vararg{ComponentVector, N}) where {N}
+    first_ax = groups[1].parameters[4].parameters[1]
+    
+    #the first parameter passed in is assumed to contain the same axes of the other groups passed in
+    layout = first_ax.parameters[1]
+    
+    exprs = []
+    for n in layout
+        push!(exprs, :(f($n, groups...)))
     end
     
     return quote
@@ -27,6 +45,10 @@ u = ComponentArray(
     molar_concentrations = (
         methylene_blue = zeros(100), 
         water = zeros(100)
+    ),
+    net_rates = (
+        MSR_rxn = 0.0,
+        WGS_rxn = 0.0
     )
 )
 
@@ -36,6 +58,9 @@ function test_foreach_field_allocations(u)
             mass_fractions[species] += 1.0
             molar_concentrations[species] += 1.0
         end
+    end
+    foreach_field_at!(u.net_rates) do rxn, net_rates
+        net_rates[rxn] += 1.0
     end
     return nothing
 end
