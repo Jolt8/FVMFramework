1b0f294e44de40e4cfcf9c00d3fe535a13c5c13e
Jolt8 <willemartin@icloud.com>
2026-03-27 12:58:17 -0600

    replaced by setup_geometry_helper_functions

---
+++ src/setup/setup_helper_functions/setup_geometry_helper_functions.jl
@@ -0,0 +1,45 @@
+function get_cellset_volume(config, set_name::String)
+    total_volume = 0.0
+    for cell_id in eachindex(config.geo.cell_volumes)
+        if cell_id in getcellset(config.grid, set_name)
+            total_volume += config.geo.cell_volumes[cell_id]
+        end
+    end
+
+    return total_volume 
+end
+
+function get_facetset_area(config, set_name::String)
+    #although there's 6 face areas per cell, all we need is the total area of all the facets of the cell that belongs in the facet_set
+    total_area = 0.0
+
+    for (cell_id, facet_idx) in getfacetset(config.grid, set_name)
+        face_area = config.geo.cell_face_areas[cell_id][facet_idx]
+        total_area += face_area
+    end
+
+    return total_area
+end
+
+#this style of function is not needed for cell volumes because it's very easy to access cell_volumes with just [cell_id]
+function get_facetset_area_per_cell(config, set_name::String)
+    n_cells = length(config.grid.cells)
+    cell_ids_respective_areas = zeros(Float64, n_cells) 
+
+    for (cell_id, facet_idx) in getfacetset(config.grid, set_name)
+        face_area = config.geo.cell_face_areas[cell_id][facet_idx]
+        cell_ids_respective_areas[cell_id] += face_area #we += because a cell might have 2 faces in this facet_set
+    end
+
+    return cell_ids_respective_areas
+end
+
+#=function get_cell_ids_in_facetset(config, set_name::String)
+    cell_ids_in_facet_set = Int[]
+
+    for (cell_id, facet_idx) in getfacetset(config.grid, set_name)
+        push!(cell_ids_in_facet_set, cell_id)
+    end
+
+    return cell_ids_in_facet_set
+end=#
