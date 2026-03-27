3ba08d4f66ffa73dac4ee528e3d3ba47b52de01e
Jolt8 <willemartin@icloud.com>
2026-03-27 12:55:54 -0600

    this was broken and I fixed it to no longer allocate

--- src/physics/physics_face_summation_functions.jl
+++ src/physics/physics_face_summation_functions.jl
@@ -1,5 +1,12 @@
 function sum_mass_flux_face_to_cell!(du, u, cell_id)
-    for face_idx in eachindex(du.mass_face[cell_id])
-        du.mass[cell_id] += du.mass_face[cell_id, face_idx]
-    end
+    #for mass_face in du.mass_face[cell_id, :]
+        #du.mass[cell_id] += mass_face
+    #end
+    #du.mass[cell_id] += sum(du.mass_face[cell_id, :])
+    #du.mass[cell_id] = du.mass_face[cell_id, 1] + du.mass_face[cell_id, 2] + du.mass_face[cell_id, 3] + du.mass_face[cell_id, 4] + du.mass_face[cell_id, 5] + du.mass_face[cell_id, 6]
+    #du.mass[cell_id] += sum(du.mass_face[cell_id, :])
+    #for i in 1:6
+        #du.mass[cell_id] += du.mass_face[cell_id, i]
+    #end
+    du.mass[cell_id] = sum(view(du.mass_face, cell_id, :))
 end
