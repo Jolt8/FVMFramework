4cce10fd971070431113588a9c2f62d71c30de17
Jolt8 <willemartin@icloud.com>
2026-03-29 21:37:13 -0600

    got rid of old stuff for debugging

--- src/physics/internal_methods/capacity_helper_functions.jl
+++ src/physics/internal_methods/capacity_helper_functions.jl
@@ -7,15 +7,6 @@ function cap_heat_flux_to_temp_change!(du, u, cell_id, vol)
     #@show u.cp[cell_id]
     du.temp[cell_id] += du.heat[cell_id] / (vol * u.rho[cell_id] * u.cp[cell_id])
     #@show du.temp[cell_id]
-    #=
-    if isnan(du.temp[cell_id])
-        println(du.temp[cell_id])
-        println("heat: ", du.heat[cell_id])
-        println("vol: ", vol)
-        println("rho: ", u.rho[cell_id])
-        println("cp: ", u.cp[cell_id])
-    end
-    =#
 end
 
 function cap_mass_flux_to_pressure_change!(du, u, cell_id, vol)
@@ -29,7 +20,7 @@ end
 function cap_species_mass_flux_to_mass_fraction_change!(du, u, cell_id, vol)
     total_mass = vol * u.rho[cell_id]
 
-    for_fields!(du.mass_fractions, u.mass_fractions,du.species_mass_flows) do species, du_mass_fractions, u_mass_fractions, species_mass_flows
+    for_fields!(du.mass_fractions, u.mass_fractions, du.species_mass_flows) do species, du_mass_fractions, u_mass_fractions, species_mass_flows
         du_mass_fractions[species[cell_id]] += (species_mass_flows[species[cell_id]] - u_mass_fractions[species[cell_id]] * du.mass[cell_id]) / total_mass
     end
 end
