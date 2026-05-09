23e2bff92ff338a2bfa1c8427ca3be7ee4ae8458
Jolt8 <willemartin@icloud.com>
2026-05-09 14:13:58 -0600

    properties of the copper mesh within the reactor

---
+++ examples/universal_packed_bed_reactor/properties/packed_media_properties/copper_mesh_reformer_properties.jl
@@ -0,0 +1,86 @@
+function get_copper_mesh_reformer_properties(pipe_length, n_cells, cell_lengths_along_pipe, wall_lengths_along_pipe)
+    van_t_hoff_A = ComponentVector(CH3O = 1.7e-6u"s^-1", HCOO = 4.74e-13u"s^-1", OH = 3.32e-14u"s^-1")
+    van_t_hoff_dH = ComponentVector(CH3O = -46800.0u"J/mol", HCOO = -115000.0u"J/mol", OH = -110000.0u"J/mol")
+
+    MSR_rxn = ComponentVector(
+        heat_of_reaction = 49500.0u"J/mol", 
+        ref_delta_G = -3800.0u"J/mol", 
+        ref_temp = 298.15u"K", 
+        kf_A = 1.59e10u"s^-1", #sources online point to values around 1.25e7 mol / (kg * s * bar)
+        kf_Ea = 103000.0u"J/mol",
+        reactant_stoich_coeffs = (methanol = 1, water = 1),
+        product_stoich_coeffs = (carbon_dioxide = 1, hydrogen = 3), 
+        stoich_coeffs = (methanol = -1, water = -1, carbon_dioxide = 1, hydrogen = 3), # CH3OH + H2O ⇋ CO2 + 3(H2)
+        van_t_hoff_A = van_t_hoff_A, 
+        van_t_hoff_dH = van_t_hoff_dH
+    )
+
+    MD_rxn = ComponentVector(
+        heat_of_reaction = 90200.0u"J/mol", 
+        ref_delta_G = 24800.0u"J/mol", 
+        ref_temp = 298.15u"K", 
+        kf_A = 1.46e13u"s^-1", #sources online point to values around 1.15e11 mol / (kg * s * bar)
+        kf_Ea = 170000.0u"J/mol",
+        reactant_stoich_coeffs = (methanol = 1,), 
+        product_stoich_coeffs = (carbon_monoxide = 1, hydrogen = 2), 
+        stoich_coeffs = (methanol = -1, carbon_monoxide = 1, hydrogen = 2), # CH3OH ⇋ CO + 2(H2)
+        van_t_hoff_A = van_t_hoff_A,
+        van_t_hoff_dH = van_t_hoff_dH
+    )
+
+    WGS_rxn = ComponentVector(
+        heat_of_reaction = -41100.0u"J/mol", 
+        ref_delta_G = -28600.0u"J/mol", 
+        ref_temp = 298.15u"K", 
+        kf_A = 4.63e10u"s^-1", #sources online point to values around 3.65e7 mol / (kg * s * bar)
+        kf_Ea = 87500.0u"J/mol",
+        reactant_stoich_coeffs = (carbon_monoxide = 1, water = 1), 
+        product_stoich_coeffs = (carbon_dioxide = 1, hydrogen = 1), 
+        stoich_coeffs = (carbon_monoxide = -1, water = -1, carbon_dioxide = 1, hydrogen = 1), # CO + H2O ⇋ CO2 + H2
+        van_t_hoff_A = van_t_hoff_A, 
+        van_t_hoff_dH = van_t_hoff_dH
+    )
+    
+    reforming_area_properties = ComponentVector(
+        k = 0.026u"W/(m*K)", 
+        cp = 4186u"J/(kg*K)",
+        dynamic_viscosity = 1.81e-5u"Pa*s",
+        rho = 998.2u"kg/m^3",
+
+        bed_void_fraction = 0.4,
+        packing_surface_area = 100.0u"m^2/m^3",
+        particle_diameter = 5.0u"mm",
+
+        saturation_temp = 72.4u"°C",
+        liquid_rho = 791.0u"kg/m^3",
+        gas_rho = 1.225u"kg/m^3",
+        mass_transfer_coeff_vap = 0.001u"kg/(m^2*s*K)",
+        heat_of_vaporization = 1.5u"kJ/g",
+
+        diffusion_coefficients = (
+            methanol = 1e-5u"m^2/s",
+            water = 1e-5u"m^2/s",
+            carbon_monoxide = 1e-5u"m^2/s",
+            hydrogen = 1e-5u"m^2/s",
+            carbon_dioxide = 1e-5u"m^2/s",
+            air = 1e-5u"m^2/s"
+        ), 
+        molecular_weights = (
+            methanol = 32.04u"g/mol",
+            water = 18.02u"g/mol",
+            carbon_monoxide = 28.01u"g/mol",
+            hydrogen = 2.02u"g/mol",
+            carbon_dioxide = 44.01u"g/mol",
+            air = 28.97u"g/mol"
+        ), 
+        reactions = (reforming_reactions = (MSR_rxn = MSR_rxn, MD_rxn = MD_rxn, WGS_rxn = WGS_rxn),),
+        reactions_kg_cat = (reforming_reactions = (MSR_rxn = 1250.0u"kg/m^3", MD_rxn = 1250.0u"kg/m^3", WGS_rxn = 1250.0u"kg/m^3"),), 
+    )
+
+    permeability = (reforming_area_properties.particle_diameter^2 * reforming_area_properties.bed_void_fraction^3) / (150.0 * (1.0 - reforming_area_properties.bed_void_fraction)^2)
+
+    reforming_area_properties = merge_properties(reforming_area_properties, ComponentVector(permeability = permeability))
+
+    return reforming_area_properties
+end
+
