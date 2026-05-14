34d814780df80bf5ce317dd17b62424a9cb86007
Jolt8 <willemartin@icloud.com>
2026-05-14 08:02:48 -0600

    these should be moved to the properties for each type of packing material

--- examples/universal_packed_bed_reactor/trials/packed_bed_reactor_water_flow_trial/water_properties.jl
+++ examples/universal_packed_bed_reactor/trials/packed_bed_reactor_water_flow_trial/water_properties.jl
@@ -10,15 +10,6 @@ function get_water_properties()
         #gas_rho = 0.4u"kg/m^3",
         #mass_transfer_coeff_vap = 0.001u"kg/(m^2*s*K)",
         #heat_of_vaporization = 2260.0u"kJ/kg",
-
-        diffusion_coefficients = (
-            methanol = 1e-5u"m^2/s",
-            water = 1e-5u"m^2/s",
-            carbon_monoxide = 1e-5u"m^2/s",
-            hydrogen = 1e-5u"m^2/s",
-            carbon_dioxide = 1e-5u"m^2/s",
-            air = 1e-5u"m^2/s"
-        ), 
     )
 
     return water_properties
