aad84e6c4419d46007adb0d65f29461c03eb2131
Jolt8 <willemartin@icloud.com>
2026-05-14 18:30:06 -0600

    updated properties slightly

--- examples/universal_packed_bed_reactor/properties/insulation_properties/insulation_properties.jl
+++ examples/universal_packed_bed_reactor/properties/insulation_properties/insulation_properties.jl
@@ -1,8 +1,8 @@
 function get_insulation_properties()
     insulation_properties = ComponentVector(
-        k = 0.15u"W/(m*K)",
-        cp = 1000.0u"J/(kg*K)",
-        rho = 100.0u"kg/m^3"
+        k = 0.10u"W/(m*K)",
+        cp = 950.0u"J/(kg*K)",
+        rho = 80.0u"kg/m^3"
     )
     return insulation_properties
 end
