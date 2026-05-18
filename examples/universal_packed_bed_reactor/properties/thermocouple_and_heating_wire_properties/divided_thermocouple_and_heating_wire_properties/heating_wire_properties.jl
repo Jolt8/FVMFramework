15107a73743ed3fbf82b15e87b482216c70962a7
Jolt8 <willemartin@icloud.com>
2026-05-17 23:47:07 -0600

    updated this to be physically correct

--- examples/universal_packed_bed_reactor/properties/thermocouple_and_heating_wire_properties/divided_thermocouple_and_heating_wire_properties/heating_wire_properties.jl
+++ examples/universal_packed_bed_reactor/properties/thermocouple_and_heating_wire_properties/divided_thermocouple_and_heating_wire_properties/heating_wire_properties.jl
@@ -5,33 +5,29 @@ insulation_k = insulation_properties.k
 insulation_cp = insulation_properties.cp
 insulation_rho = insulation_properties.rho
 
-function get_heating_wire_properties(grid, pipe_length, n_cells_axial, cell_lengths_along_pipe)
-
-    air_k = 0.0257u"W/(m*K)"
-    air_cp = 1.007u"J/(g*K)"
-    air_rho = 1.225u"kg/m^3"
-
-    #we may acutally want to use the properties of the insulation because it was wrapped on top of the heating wire, 
-    #filling the gaps between the wires
-
+function get_heating_wire_properties(grid, pipe_length, common_properties)
     kanthal_heating_wire_k = 18.0u"W/(m*K)"
     kanthal_heating_wire_cp = 500.0u"J/(kg*K)"
     kanthal_heating_wire_rho = 7800.0u"kg/m^3"
 
     heating_wire_spacing_between_turns = 10.0u"inch" / 30.6 
-    heating_wire_thickness = 0.7u"mm" #thickness of the heating wire
-    heating_wire_length = 2.7432u"m"
+    heating_wire_diameter = common_properties.heating_wire_outside_diameter - common_properties.heating_wire_inside_diameter
+    heating_wire_length = 2.7432u"m" #length of heating wire actually used
+    heating_section_length = 10.0u"inch" #length along the pipe the heating wire is coiled around on
 
-    heating_wire_volume = pi * heating_wire_thickness^2 / 4 * heating_wire_length
-    heating_section_volume = pi * (common_properties.thermocouple_region_outside_diameter/2)^2 * pipe_length - 
-        pi * (common_properties.insulation_inside_diameter/2)^2 * pipe_length
+    heating_wire_volume = pi * (heating_wire_diameter/2)^2 * heating_wire_length 
 
-    heating_wire_section_non_heater_void = (heating_section_volume - heating_wire_volume) / heating_section_volume
+    total_heating_section_volume = pi * (common_properties.heating_wire_outside_diameter/2)^2 * common_properties.pipe_length - 
+        pi * (common_properties.heating_wire_inside_diameter/2)^2 * common_properties.pipe_length
+    #the heating wire is only coiled for 10 inches along the pipe, so the 1 inch on the bottom and top of the pipe are filled in with the 
+    #total heating section volume
+
+    heating_wire_section_non_heater_void = (total_heating_section_volume - heating_wire_volume) / total_heating_section_volume
 
     heating_wire_properties = ComponentVector(
-        k = heating_wire_section_non_heater_void * air_k + (1 - heating_wire_section_non_heater_void) * kanthal_heating_wire_k,
-        cp = heating_wire_section_non_heater_void * air_cp + (1 - heating_wire_section_non_heater_void) * kanthal_heating_wire_cp,
-        rho = heating_wire_section_non_heater_void * air_rho + (1 - heating_wire_section_non_heater_void) * kanthal_heating_wire_rho,
+        k = heating_wire_section_non_heater_void * insulation_k + (1 - heating_wire_section_non_heater_void) * kanthal_heating_wire_k,
+        cp = heating_wire_section_non_heater_void * insulation_cp + (1 - heating_wire_section_non_heater_void) * kanthal_heating_wire_cp,
+        rho = heating_wire_section_non_heater_void * insulation_rho + (1 - heating_wire_section_non_heater_void) * kanthal_heating_wire_rho,
     )
 
     return heating_wire_properties
