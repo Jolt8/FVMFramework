c5556a3411064b699c2f20265317ca7ef560dc78
Jolt8 <willemartin@icloud.com>
2026-05-28 14:31:36 -0600

    updated this to be less bad

--- src/physics/flux_methods/flux_physics/heat_transfer.jl
+++ src/physics/flux_methods/flux_physics/heat_transfer.jl
@@ -1,16 +1,12 @@
-function get_k_effective(k_a, k_b)
-    return 2 * k_a * k_b / (k_a + k_b)
-end
-
 function heat_diffusion!(
     du, u, 
     idx_a, idx_b, face_idx,
-    area, norm, dist
+    area, norm, dist,
+    vol_a, vol_b
 )
-    k_effective = get_k_effective(u.k[idx_a], u.k[idx_b])
+    k_effective = 2 * u.k[idx_a] * u.k[idx_b] / (u.k[idx_a] + u.k[idx_b])
 
     grad_T = (u.temp[idx_b] - u.temp[idx_a]) / dist
-    F = -k_effective * grad_T * area
 
-    du.heat[idx_a] -= F
+    du.heat[idx_a] -= -k_effective * grad_T * area
 end
