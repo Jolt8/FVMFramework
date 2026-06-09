d7ec113352ea2d43887cc80081e8de23f7194bc5
Jolt8 <willemartin@icloud.com>
2026-06-08 19:33:32 -0600

    small function signature changes

--- examples/acoustic_imaging_with_raytracing/transducer_profiles/ray_mapping/generate_ray_map.jl
+++ examples/acoustic_imaging_with_raytracing/transducer_profiles/ray_mapping/generate_ray_map.jl
@@ -1,4 +1,4 @@
-function generate_ray_map_between_nodes(transducer_ids, node_ids, grid, geo)
+function generate_ray_map_between_nodes(transducer_ids, transducer_node_ids, grid, geo)
     ray_map_intersected_cells = [Int[] for i in 1:length(transducer_ids), j in 1:length(transducer_ids)]
     ray_map_distances_through_cells = [Float64[] for i in 1:length(transducer_ids), j in 1:length(transducer_ids)]
     ray_map_ray_lengths = zeros(Float64, length(transducer_ids), length(transducer_ids))
@@ -9,7 +9,7 @@ function generate_ray_map_between_nodes(transducer_ids, node_ids, grid, geo)
             destination_node_id = transducer_node_ids[destination_transducer_id]
 
             if origin_transducer_id != destination_transducer_id #I'm lazy, so we're not slicing the array
-                intersected_cells, corresponding_intersection_lengths, ray_length = find_traced_cells(origin_node_id, destination_node_id, grid)
+                intersected_cells, corresponding_intersection_lengths, ray_length = find_traced_cells(origin_node_id, destination_node_id, grid, geo)
 
                 ray_map_intersected_cells[origin_transducer_id, destination_transducer_id] = intersected_cells
                 ray_map_distances_through_cells[origin_transducer_id, destination_transducer_id] = corresponding_intersection_lengths
@@ -21,8 +21,8 @@ function generate_ray_map_between_nodes(transducer_ids, node_ids, grid, geo)
 end
 
 
-function generate_ray_map(transducer_ids, node_ids, grid, geo)
-    ray_map_intersected_cells, ray_map_distances_through_cells, ray_map_ray_lengths = generate_ray_map_between_nodes(transducer_ids, node_ids, grid, geo)
+function generate_ray_map(transducer_ids, transducer_node_ids, grid, geo)
+    ray_map_intersected_cells, ray_map_distances_through_cells, ray_map_ray_lengths = generate_ray_map_between_nodes(transducer_ids, transducer_node_ids, grid, geo)
     
     save_object(joinpath(
             @__DIR__, 
