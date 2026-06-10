cd1b282b8b27271006997a1fec167b2fc28a1255
Jolt8 <willemartin@icloud.com>
2026-06-10 14:32:24 -0600

    updated to match new flux function function signatures

--- examples/universal_packed_bed_reactor/physics/convective_heat_transfer.jl
+++ examples/universal_packed_bed_reactor/physics/convective_heat_transfer.jl
@@ -1,7 +1,8 @@
 function fluid_to_steel_pipe_convective_heat_transfer!(
     du, u, 
     idx_a, idx_b, face_idx,
-    area, norm, dist
+    area, norm, dist,
+    vol_a, vol_b
 )
     du.heat[idx_a] += u.fluid_to_steel_pipe_convective_heat_transfer_coefficient[idx_a] * (u.temp[idx_b] - u.temp[idx_a]) * area
 end
