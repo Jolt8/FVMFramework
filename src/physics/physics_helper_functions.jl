aa1b0ebb5e953e262a6183ec3f01d2fe04de2097
Jolt8 <willemartin@icloud.com>
2026-07-09 20:24:38 -0600

    got rid of whatever the hell this was changed for

--- src/physics/physics_helper_functions.jl
+++ src/physics/physics_helper_functions.jl
@@ -38,10 +38,6 @@ function mw_avg!(u, cell_id)
         inv_mw += mass_fractions[species[cell_id]] / molecular_weights[species[cell_id]]
     end
 
-    @show u.mw_avg[cell_id]
-
-    u.mw_avg[1] = 1.0u"g/mol"
-    
     u.mw_avg[cell_id] = 1.0 / inv_mw
 end
 
